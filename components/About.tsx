20123c68685d1f8e77b9fe3c5368ade82590580c
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2024-09-06 23:45:28 +0530

    update: add About section

---
+++ components/About.tsx
@@ -0,0 +1,31 @@
+const About = () => {
+  return (
+    <div className="text-lg flex flex-col gap-5 p-5 text-neutral-400">
+      <p>
+        I am a frontend developer, passionate about creating clean, intuitive
+        web applications. I specialize in{" "}
+        <span className="text-yellow-200">React</span> and{" "}
+        <span className="text-yellow-200">JavaScript</span>.
+      </p>
+      <p>
+        Currently, at <span className="text-yellow-200">Businessnext</span>, I
+        develop user interfaces for our customer-centric platform, enabling the
+        seamless creation of customizable features and crafting dynamic user
+        experiences.
+      </p>
+      <p>
+        I’m driven by curiosity, always looking for better ways to bring ideas
+        to life through code and constantly exploring new ways to improve user
+        experiences.
+      </p>
+      <p>
+        When I’m not coding, I love exploring new{" "}
+        <span className="text-yellow-200">languages</span>, getting into{" "}
+        <span className="text-yellow-200">anime</span>, and losing myself in{" "}
+        <span className="text-yellow-200">music</span>.
+      </p>
+    </div>
+  );
+};
+
+export default About;
--- components/MainComponent.tsx
+++ components/MainComponent.tsx
@@ -1,17 +1,29 @@
+import { MdArrowOutward } from "react-icons/md";
+
 import Experience from "./Experience";
+import Projects from "./Projects";
+import About from "./About";
 
 const MainComponent = () => {
   return (
-    <main className="w-[50%] relative left-[300px]">
-      <div id="about" className="text-xl">
-        <h3>About</h3>
-      </div>
+    <main className="w-[50%] relative left-[300px] flex flex-col gap-16">
+      <About />
 
       <Experience />
+      <a
+        href={
+          "https://drive.google.com/file/d/1Fk2HBPrkhg5um22zMfCRPnS-Bg39XXlQ/view?usp=drive_link"
+        }
+        target="_blank"
+        className="flex items-center gap-1 text-xl p-5"
+      >
+        <span>View Full Resume</span>
+        <span>
+          <MdArrowOutward className="arrow-upward" />
+        </span>
+      </a>
 
-      <div id="projects" className="text-xl">
-        <h3>Projects</h3>\
-      </div>
+      <Projects />
     </main>
   );
 };
