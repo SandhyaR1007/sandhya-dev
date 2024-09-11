71782768c52f4690971db385b7a4f638b85bd13f
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2024-09-11 21:50:34 +0530

    update: footer alignment

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="pt-10">
+    <div className="pt-10 text-center lg:text-left">
       <p className="text-neutral-400">
         Built with Next.js and Tailwind. Made with 💛 by{" "}
         <span className="text-white">Sandhya</span>.
